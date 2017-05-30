104660efce4ffc5b7321e903ed6c6bb07a5d6704
Stevenzwzhai <zzwdzsw@sina.cn>
2017-05-30 23:24:20 +0800

    pwa

---
+++ pwa/sw.js
@@ -0,0 +1,16 @@
+const catchVersion = "v1";
+
+const catchList = [
+	'/',
+	'/dist/build.js',
+	'/dist/svg/Categeory.svg',
+	'/dist/svg/add.svg',
+	'/dist/svg/arrow.svg',
+	'/dist/svg/download.svg',
+	'/dist/svg/home.svg',
+	'/dist/svg/more-x.svg',
+	'/dist/svg/more-y.svg',
+	'/dist/svg/notify.svg',
+	'/dist/svg/star.svg',
+	'/dist/svg/user.svg'
+]
